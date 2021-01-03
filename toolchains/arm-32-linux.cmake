f42d3dd3c50ea3dc9044c2af8ade802b6ee24c5c
Alex Reinking <alex.reinking@gmail.com>
2021-01-03 01:02:41 -0800

    Try to get ARM Linux working.

---
+++ toolchains/arm-32-linux.cmake
@@ -0,0 +1,8 @@
+set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_PROCESSOR ARM)
+
+set(CMAKE_C_COMPILER arm-linux-gnueabihf-gcc)
+set(CMAKE_CXX_COMPILER arm-linux-gnueabihf-g++)
+
+set(LLVM_TARGET_ARCH ARM)
+set(LLVM_DEFAULT_TARGET_TRIPLE arm-linux-gnueabihf)
---
+++ toolchains/arm-64-linux.cmake
@@ -0,0 +1,8 @@
+set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_PROCESSOR aarch64)
+
+set(CMAKE_C_COMPILER aarch64-linux-gnu-gcc)
+set(CMAKE_CXX_COMPILER aarch64-linux-gnu-g++)
+
+set(LLVM_TARGET_ARCH AArch64)
+set(LLVM_DEFAULT_TARGET_TRIPLE aarch64-linux-gnu)
